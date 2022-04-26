01764f882e752d6ef4f149948a5b547eecdc7f1f
Letmeng <981682720@qq.com>
2022-04-26 23:20:26 +0800

    Third git commit(修改)

--- src/main/java/com/letmeng/sys/test/test.java
+++ src/main/java/com/letmeng/sys/test/test.java
@@ -3,5 +3,6 @@ package com.letmeng.sys.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git2!");
     }
 }
